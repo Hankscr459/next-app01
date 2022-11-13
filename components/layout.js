cae9afdb43bf4421532088ab376837029349c8de
Hankscr459 <defcw58@gmail.com>
2022-11-13 10:54:12 +0800

    feat: add material ui

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Navbar from 'components/layout/navbar';
 import Footer from 'components/layout/footer';
-import { get } from 'lodash';
 
 const Layout = ({ children }) => {
-  const a = { value: 7777 };
   return pug`
     div
       Navbar
-      p ${get(a, 'value', 'default')}
       main 
         ${children}
       Footer
