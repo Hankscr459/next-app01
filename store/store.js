bb564ce81a2a36d0841345b4a88e2b95912927f9
hank <akysn564@gmail.com>
2022-11-14 11:19:56 +0800

    feat: customizationReducer

--- store/store.js
+++ store/store.js
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { authSlice } from './authSlice';
+import { customization } from './customization';
 import { createWrapper } from 'next-redux-wrapper';
 
 const makeStore = () =>
   configureStore({
     reducer: {
       [authSlice.name]: authSlice.reducer,
+      [customization.name]: customization.reducer,
     },
     devTools: true,
   });
