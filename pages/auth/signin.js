7161e667daf7efefc656efa91f5c82907181b760
hank <akysn564@gmail.com>
2022-11-23 09:53:40 +0800

    test: test api message

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@mui/material';
 import { Api } from '@/plugins/api';
-import { to } from 'await-to-js';
+import { Card } from '@mui/material';
 
 export default function Signin() {
+  const [token, setToken] = useState({});
+  const [errMessage, setErrMessage] = useState({});
+
   const signin = async() => {
-    const [err, res] = await to(Api.user.signin());
-    if (err) {
-      console.log('err: ', err);
+    const res = await Api.user.signin();
+    if (res.success) {
+      setToken(res);
     }
-    console.log('res: ', res);
+    if (!res.success) {
+      setErrMessage(res.message);
+    }
   }
   return pug`
-    div
-      Button(size="small", onClick=${signin}) Learn More
+    div(className="flex justify-center")
+      .flex-col
+        Button(size="small", onClick=${signin}) Learn More
+        h3 Err: ${JSON.stringify(errMessage)}
+        h3 success: ${JSON.stringify(token)}
+    // div(className="flex justify-center")
+    //   .flex-col
+    //     Button(size="small", onClick=${signin}) Learn More
+    //     br
+    //     Card(className="w-[30rem] flex justify-center")
+    //       div(className="flex-col")
+    //         h2 title
+    //         span(
+    //           className="flex max-w-[20rem] overflow-y-auto",
+    //         ) value: ${JSON.stringify(token)}
   `
 }
--- plugins/api.js
+++ plugins/api.js
@@ -1,6 +1,9 @@
 import { useCookie } from 'react-use';
 
 const apiUrl = process.env.ApiUrl || '';
+const stringify = (obj) => {
+  return { body: JSON.stringify(obj) };
+}
 
 export const Api = {
   // const [value, updateCookie, deleteCookie] = useCookie('my');
@@ -10,7 +13,7 @@ export const Api = {
     signin: async() => {
       return await (await fetch(`${apiUrl}/v1/user/signin`, {
         method: 'POST',
-        body: JSON.stringify({
+        ...stringify({
           email: 'Alan@gmail.com',
           password: '123456789',
         }),
