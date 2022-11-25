67d0f8697ab197fc8f9816fa54f2267226dbcdde
hank <akysn564@gmail.com>
2022-11-25 11:59:20 +0800

    fixed: server api error

--- plugins/api.js
+++ plugins/api.js
@@ -5,19 +5,29 @@ const stringify = (obj) => {
   return { body: JSON.stringify(obj) };
 }
 
+const fetchApi = async(url, opts) => {
+  const [err, res] = await to(fetch(url, opts));
+  if (res) {
+    return await res.json();
+  }
+  if (err) {
+    return err;
+  }
+};
+
 export const Api = {
   // const [value, updateCookie, deleteCookie] = useCookie('my');
   // console.log('value: ', value)
   
   user: {
     signin: async() => {
-      return await (await fetch(`${apiUrl}/v1/user/signin2`, {
+      return await fetchApi(`${apiUrl}/v1/user/signin`, {
         method: 'POST',
         ...stringify({
           email: 'Alan@gmail.com',
           password: '123456789',
         }),
-      })).json();
+      });
     },
   },
 };
